1ca6c076209972c12d3967b1a5741f6419db8141
eprincev.egor@yandex.ru <eprincev.egor@yandex.ru>
2015-10-27 16:54:23 +0300

    сверстал пару секций

--- js/classes/MainApp.js
+++ js/classes/MainApp.js
@@ -3,17 +3,25 @@ define([
 	"eva",
 	"jquery"
 ], function(f, Events, $) {
-	
+
 	var MainApp = f.CreateClass("MainApp", {}, Events);
-	
+
 	MainApp.prototype.init = function(params) {
 		params = params || {};
 		for (var key in params) {
 			this[key] = params[key];
 		}
-		
+
 		// some inits...
+		this.$header = $(".header");
+		$(window).on("scroll", this.onScroll.bind(this));
+		this.onScroll();
 	};
-	
+
+	MainApp.prototype.onScroll = function() {
+		var scroll = f.getScroll();
+		this.$header.toggleClass("sticky", scroll.top > 100);
+	};
+
 	return MainApp;
 })
